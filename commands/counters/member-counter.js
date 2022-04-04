7962d59502735f152555b78f0df7eb2619f4b810
CADIndie <84008186+CADIndie@users.noreply.github.com>
2022-04-04 13:29:42 -0400

    Update member-counter.js

--- commands/counters/member-counter.js
+++ commands/counters/member-counter.js
@@ -1,1 +1,9 @@
-
+module.exports = async (client) =>{
+    const guild = client.guilds.cache.get('956951770205143041');
+    setInterval(() =>{
+        const memberCount = guild.memberCount;
+        const channel = guild.channels.cache.get('958391872131330088');
+        channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
+        console.log('Updating Member Count');
+    }, 5000);
+};
